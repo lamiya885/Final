273677eb32945a211b3aa5bf3d1c7fe903d7fa9c
lamiya885 <lamiyabh-bp215@code.edu.az>
2025-02-23 23:52:26 +0400

    Stripe0.2

--- Final#CFF.API/Program.cs
+++ Final#CFF.API/Program.cs
@@ -106,34 +106,34 @@ namespace Final_CFF.API
                 });
             }
 
-            //app.UseExceptionHandler(
-            //    opt =>
-            //    {
-            //        opt.Run(async context =>
-            //        {
-            //            var feature = context.Features.GetRequiredFeature<ExceptionHandlerFeature>();
-            //            var exception = feature.Error;
-            //            if (exception is IBaseException bEx)
-            //            {
-            //                context.Response.StatusCode = bEx.StatusCode;
-            //                await context.Response.WriteAsJsonAsync(new
-            //                {
-            //                    Message = bEx.ErrorMessage
-            //                });
+            app.UseExceptionHandler(
+                opt =>
+                {
+                    opt.Run(async context =>
+                    {
+                        var feature = context.Features.GetRequiredFeature<ExceptionHandlerFeature>();
+                        var exception = feature.Error;
+                        if (exception is IBaseException bEx)
+                        {
+                            context.Response.StatusCode = bEx.StatusCode;
+                            await context.Response.WriteAsJsonAsync(new
+                            {
+                                Message = bEx.ErrorMessage
+                            });
 
-            //            }
-            //            else
-            //            {
-            //                context.Response.StatusCode = 400;
-            //                await context.Response.WriteAsJsonAsync(new
-            //                {
-            //                    Message = "Bir xeta bas verdi!"
-            //                });
-            //            }
-            //        }
-            //            );
-            //    }
-            //);
+                        }
+                        else
+                        {
+                            context.Response.StatusCode = 400;
+                            await context.Response.WriteAsJsonAsync(new
+                            {
+                                Message = "Bir xeta bas verdi!"
+                            });
+                        }
+                    }
+                        );
+                }
+            );
 
             app.UseHttpsRedirection();
 
